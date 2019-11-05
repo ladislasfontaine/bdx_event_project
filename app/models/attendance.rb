b99ccb724ea428787bfa7b0893fa697cafa047bf
ladislasfontaine <fontaine.ladislas@gmail.com>
2019-11-05 18:06:24 +0100

    Edit.

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,7 +2,7 @@ class Attendance < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  after_create :creator_congrats
+  # after_create :creator_congrats
 
   def creator_congrats
     UserMailer.new_participant(self).deliver_now
